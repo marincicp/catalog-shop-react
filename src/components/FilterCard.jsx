9c8495445298f46d4071f8a248969666d059f2ee
Petar <marincicpetar@gmail.com>
2024-11-14 15:00:02 +0100

    ui fix

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -18,7 +18,7 @@ function FilterCard({
   const hasQueryParams = location.search.length > 0;
   return (
     <div className="flex justify-center items-start flex-col px-8 py-4 border-b-2 border-b-gray-100">
-      <div className="flex items-center">
+      <div className="flex items-center max-h-6 ">
         <p className={`font-bold tracking-wide ${titleSizes[titleSize]}`}>
           {title}
         </p>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,7 +5,7 @@ function ProductList({ data = [], isLoading, error }) {
 
   if (error) return <div>No data to show</div>;
   return (
-    <div className="bg-white w-full h-screen grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ">
+    <div className="bg-white w-full h-fit grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6  ">
       {data?.map((item) => (
         <ProductCard key={item.id} item={item} />
       ))}
